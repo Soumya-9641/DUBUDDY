5e94b08109e37f84ca79698ddbdd724ee45788e1
Soumya-9641 <soumyadipgantait2003@gmail.com>
2024-04-27 11:38:22 +0530

    changes

--- client/src/basUrl.js
+++ client/src/basUrl.js
@@ -1,3 +1,4 @@
-const baseURL = 'http://localhost:9000'; // Replace with your actual base URL
+//const baseURL = 'http://localhost:9000';
+const baseURL = 'https://dubuddy.onrender.com';  // Replace with your actual base URL
 
 export default baseURL;
